b2ae4bf72c09452329c05728f4176ea9ef8891d8
charlesaguiar93 <charlesaguiar93@gmail.com>
2025-09-22 21:41:00 -0300

    projeto-adicionar-tarefas

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
+
+let tarefas = [];
+
+
 function adicionnartarefa(){
 
+    const inputTarefa = document.getElementById("inputTarefa")      
+    let tarefa = inputTarefa.value.trim()
 
 
 
-
-
-
-
-
-
-
-
-
-    
 }
