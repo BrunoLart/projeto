69c678da2a88873e42cd921f2543ff96716ff9dc
charlesaguiar93 <charlesaguiar93@gmail.com>
2025-09-22 21:52:44 -0300

    projeto-adicionar-tarefas

--- script.js
+++ script.js
@@ -6,7 +6,22 @@ function adicionnartarefa(){
 
     const inputTarefa = document.getElementById("inputTarefa")      
     let tarefa = inputTarefa.value.trim()
+    
+    const mensagem = document.getElementById("mensagem")
 
+    if(tarefa == ""){
 
+        let mensagemErro = "Digite uma tarefa para sua lista"
+        mensagem.textContent = mensagemErro; 
+    }else{
+
+        let mensagemSucesso = "Tarefa adicionada com sucesso!";
+        mensagem.textContent = mensagemSucesso;   
+        tarefas.push(tarefa)
+        renderizarTarefas() 
+
+    } 
+
+    inputTarefa.value = ""
 
 }
